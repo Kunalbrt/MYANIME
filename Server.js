17204723c56dd677bcc19d8941d24c6013e61bce
kunalnordy <bhartikunal886@gmail.com>
2026-05-20 17:57:00 +0530

    fix: update CORS for cloudflare workers URL

--- Server.js
+++ Server.js
@@ -43,7 +43,7 @@ app.use(cors({
     process.env.FRONTEND_URL,
     'http://localhost:3000',
     'http://localhost:5173',
-    'https://watchmyanime.netlify.app'
+    'https://muddy-leaf-827b.bhartikunal886.workers.dev'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
