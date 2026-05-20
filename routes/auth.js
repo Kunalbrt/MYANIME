470457c6e12c578efba95a176e4536cc30a228f6
kunalnordy <bhartikunal886@gmail.com>
2026-05-20 07:07:52 +0530

    fix auth routes

--- routes/auth.js
+++ routes/auth.js
@@ -14,9 +14,5 @@ router.get('/me', protect, getMe);
 
 module.exports = router;
 
-const express = require('express');
-const router = express.Router();
 
-router.get('/test', (req, res) => res.json({ message: 'auth placeholder route works!' }));
 
-module.exports = router;
