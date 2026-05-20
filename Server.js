c907a73cbf246f1e64eadb7f69043ce298550a73
kunalnordy <bhartikunal886@gmail.com>
2026-05-20 10:13:54 +0530

    fix: add netlify URL to CORS

--- Server.js
+++ Server.js
@@ -42,7 +42,8 @@ app.use(cors({
   origin: [
     process.env.FRONTEND_URL,
     'http://localhost:3000',
-    'http://localhost:5173'
+    'http://localhost:5173',
+    'https://watchmyanime.netlify.app'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
